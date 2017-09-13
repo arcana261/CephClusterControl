04d054e6a82bfb81fdcbd95c7a98087f177fbcff
Mohamad mehdi Kharatizadeh <m_kharatizadeh@yahoo.com>
2017-09-13 18:22:29 +0430

    working on samba share client

--- mock/server.js
+++ mock/server.js
@@ -1,22 +1,15 @@
 "use strict";
 
-const ServerLoop = require('../lib/rpc/ServerLoop');
+const Samba = require('../lib/samba');
 
 async function main() {
-  let server = new ServerLoop('amqp://localhost?heartbeat=10', 'sample');
+  const smb = new Samba();
 
-  await server.start();
-
-  server.addHandler('mehdi', {
-    hello: function() {
-      return 'hello, world!';
-    }
-  });
-
-  await server.addType('info');
+  await smb._adduser('smb_mehdix');
 }
 
 main().catch(err => {
-  console.log('[ERR :(]\n', err);
+  console.log(err);
   process.exit(-1);
 });
+
