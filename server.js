d2ed70d0fccaed5a4ee7ead4dbf721333a697e41
Mohamad mehdi Kharatizadeh <m_kharatizadeh@yahoo.com>
2017-09-18 14:16:31 +0430

    added mounting to iscsi addBackStore

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 "use strict";
 
+/*
+docker pull rabbitmq:latest
+docker run -td --restart=always --name rabbitmq --hostname rabbitmq -p 5671:5671 -p 5672:5672 -p 4369:4369 -p 25672:25672 rabbitmq:latest
+ */
+
 const ServerLoop = require('./lib/rpc/ServerLoop');
 const CephClient = require('./lib/ceph');
 const RbdClient = require('./lib/rbd');
