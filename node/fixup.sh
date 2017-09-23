1387fdbc595cb0140675c859fe4f6aaab0b0f4a5
Mohamad mehdi Kharatizadeh <m_kharatizadeh@yahoo.com>
2017-09-23 12:35:45 +0330

    added man pages

--- node/fixup.sh
+++ node/fixup.sh
@@ -2,4 +2,13 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+/bin/chmod +x "${DIR}/bin/n"
+/bin/chmod +x "${DIR}/bin/n-uninstall"
+/bin/chmod +x "${DIR}/bin/n-update"
+/bin/chmod +x "${DIR}/bin/node"
+/bin/ln -s "${DIR}/lib/node_modules/npm/bin/npm-cli.js" "${DIR}/bin/npm"
+/bin/ln -s "${DIR}/lib/node_modules/npm/bin/npx-cli.js" "${DIR}/bin/npx"
+/bin/chmod +x "${DIR}/bin/npm"
+/bin/chmod +x "${DIR}/bin/npx"
+
 echo "Hello, ${DIR}!"
