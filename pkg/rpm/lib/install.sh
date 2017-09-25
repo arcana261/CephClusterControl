c82ee7dab08e8806d7e33a1b8c824733ab818288
Mohamad mehdi Kharatizadeh <m_kharatizadeh@yahoo.com>
2017-09-25 09:20:44 +0330

    made small change to rpm build script

--- pkg/rpm/lib/install.sh
+++ pkg/rpm/lib/install.sh
@@ -3,11 +3,9 @@ rm -rf %{buildroot}
 mkdir -p %{buildroot}
 
 mkdir -p %{buildroot}/var/lib/kaveh-cluster-ctrl
-mkdir -p %{buildroot}/usr/local/bin
 mkdir -p %{buildroot}/usr/local/lib/kaveh-cluster-ctrl
-mkdir -p %{buildroot}/lib/systemd/system
 
-cp -rf etc %{buildroot}/
+cp -rf etc %{buildroot}/usr/local/lib/kaveh-cluster-ctrl/
 cp -rf node %{buildroot}/usr/local/lib/kaveh-cluster-ctrl/
 cp -rf lib %{buildroot}/usr/local/lib/kaveh-cluster-ctrl/
 cp -rf config %{buildroot}/usr/local/lib/kaveh-cluster-ctrl/
@@ -17,7 +15,7 @@ cp -f README.md %{buildroot}/usr/local/lib/kaveh-cluster-ctrl/
 cp -f package.json %{buildroot}/usr/local/lib/kaveh-cluster-ctrl/
 cp -f package-lock.json %{buildroot}/usr/local/lib/kaveh-cluster-ctrl/
 cp -rf node_modules %{buildroot}/usr/local/lib/kaveh-cluster-ctrl/
-cp -rf bin %{buildroot}/usr/local/
-cp -f systemd/kaveh-cluster-ctrl.service %{buildroot}/lib/systemd/system/
+cp -rf bin %{buildroot}/usr/local/lib/kaveh-cluster-ctrl/
+cp -rf systemd %{buildroot}/usr/local/lib/kaveh-cluster-ctrl/
 
 
